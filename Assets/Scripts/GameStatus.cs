dabed43e43462bf1c6bcd73b917ec0e6fe5c5d09
Gerald <gprmarquez@gmail.com>
2020-10-29 11:09:34 -0700

    removing menu background. adding GameStatus for persisting score

---
+++ Assets/Scripts/GameStatus.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStatus : MonoBehaviour
+{
+    public int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = PlayerPrefs.GetInt("score", 0);
+    }
+
+    void OnDestroy()
+    {
+        PlayerPrefs.SetInt("score", score);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
